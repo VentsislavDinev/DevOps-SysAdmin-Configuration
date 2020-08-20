a8424a8c65b58cf8565d3d313101153052f2bb65
Ventsislav Dinev <vdinev94@hotmail.com>
2020-08-20 23:14:46 +0300

    Update and install sample package for Debian server
    Update all package.
    Install ngInx web server.
    Install http2 web server
    Set permission for /var/www directory.

---
+++ Bash/configuration.sh
@@ -0,0 +1,14 @@
+# Update all modules on the machine
+sudo apt-get update
+
+
+# install ngInx package. 
+sudo apt-get install nginx -y
+
+// again update all packages. 
+sudo apt-get update -y
+
+sudo apt-get instal httpd2 -y 
+
+// configure directory with permission 
+chmod -R 777 /var/www
